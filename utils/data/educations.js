7ae31716723fce7b626b61af1f0129bbd1475e80
GhulamMohiudinN <mohiudin7174@gmail.com>
2024-04-05 23:57:46 +0500

    initial commit

---
+++ utils/data/educations.js
@@ -0,0 +1,20 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Bachelor Degree",
+    duration: "2022 - Present",
+    institution: "University Of Okara",
+  },
+  {
+    id: 2,
+    title: "Higher Secondary Certificate",
+    duration: "2018 - 2020",
+    institution: "Punjab Group of Collages Okara",
+  },
+  {
+    id: 3,
+    title: "Secondary School Certificate",
+    duration: "2007 - 2018",
+    institution: "District Public School Okara",
+  },
+];
---
+++ utils/data/personal-data.js
@@ -0,0 +1,19 @@
+export const personalData = {
+  name: "Ghulam Mohiudin",
+  profile: "/profile.jpg",
+  designation: "Front-End Developer",
+  description:
+    "My name is Ghulam Mohiudin. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: "mohiudin7174@gmail.com",
+  phone: "+923227028560",
+  address: "Okara, Punjab, Pakistan",
+  github: "https://github.com/GhulamMohiudinN",
+  facebook: "https://www.linkedin.com/in/ghulam-mohiuddin-b46378250/",
+  linkedIn: "https://www.linkedin.com/in/ghulam-mohiuddin-b46378250/",
+  twitter: "https://www.linkedin.com/in/ghulam-mohiuddin-b46378250/",
+  stackOverflow: "https://www.linkedin.com/in/ghulam-mohiuddin-b46378250/",
+  leetcode: "https://www.linkedin.com/in/ghulam-mohiuddin-b46378250/",
+  devUsername: "GhulamMohiudin",
+  resume:
+    "https://docs.google.com/document/d/19Hh2P4IDDr2pXKAUkPLHq2BQAwDP2tZSzdeW2aJx5DY/edit?usp=sharing",
+};
